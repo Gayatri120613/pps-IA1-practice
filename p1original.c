4f9adbafac84cfbae10630e6e74e165a3306c010
gayatri120613 <94883116+Gayatri120613@users.noreply.github.com>
2022-02-02 09:25:23 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include <stdio.h>
+int input(int *pa, int *pb)
+{
+  printf("enter two numbers\n");
+  scanf("%d%d",pa, pb);
+}
+int add(int a, int b, int *sum)
+{
+  *sum=a+b;
+}
+int output(int a, int b, int sum)
+{
+  printf("the sum of %d+%d is %d",a,b,sum);
+}
+int main()
+{
+  int a, b, c;
+  input(&a,&b);
+  add(a,b,&c);
+  output(a,b,c);
+  return 0;
 }
